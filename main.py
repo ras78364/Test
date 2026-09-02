f47eaa1a01104f82a8a1b6f1608014f997125db0
ras78364 <ras0226653@ju.edu.jo>
2026-09-02 15:31:56 +0300

    Change login text on feature branch

--- main.py
+++
@@ -1,9 +0,0 @@
-prices = {
-    "Store_A": {"Apple": 0.50, "Banana": 0.30},
-    "Store_B": {"Apple": 0.60, "Banana": 0.25},
-    "Store_C": {"Apple": 0.55, "Banana": 0.35}
-}
-for id, info in prices.items():
-    print("Store:",id)
-    for x in info:
-        print(x, ":",info[x])
